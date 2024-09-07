e5990c9c505820e53da3309a60fc9d97eb06683c
Fatma A <fka201520@gmail.com>
2024-09-07 09:26:30 +0300

    packages installed

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,8 @@
-import React from 'react';
-export function App() {
-  return <div>hello world</div>;
+import React from "react";
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-200">
+      Hello world!
+    </h1>
+  )
 }
